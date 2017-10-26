b85a039dd74a0cc575a76d751211529913a439cd
bigz <493971184@qq.com>
2017-10-26 14:16:00 +0800

    5-4 Vue-router上

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
+
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 /* eslint-disable no-new */
-new Vue({
+/*new Vue({
   el: 'body',
   components: { App }
+})*/
+
+let app = Vue.extend(App)
+let router = new VueRouter()
+router.map({
+  '/goods':{
+    component:goods
+  },
+  '/ratings':{
+    component:ratings
+  },
+  '/seller':{
+    component:seller
+  }
 })
+
+
+router.start(app,'#app')
