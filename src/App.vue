e676e757dcd66c35b5669e3470fc58baaabd557f
bigz <493971184@qq.com>
2017-10-27 09:50:18 +0800

    6-2 vue-resource应用

--- src/App.vue
+++ src/App.vue
@@ -13,29 +13,46 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import header from 'components/header/header.vue'
-
+  const ERR_OK = 0
   export default {
+    data(){
+      return {
+        seller: {}
+      }
+    },
+    created(){
+      this.$http.get('/api/seller').then((res)=>{
+        res = res.body
+          if(res.errno === ERR_OK){
+              this.seller = res.data
+            console.log(this.seller);
+          }
+      })
+    },
     components: {
       'v-header': header
+    },
+    methods:{
+
     }
   }
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus" type="text/stylus">
   @import "./common/stylus/mixin.styl"
-    .tab
-      display: flex
-      width: 100%
-      height: 40px
-      line-height: 40px
-      border-1px(rgba(7,17,27,0.1))
-      .tab-item
-        flex: 1
-        text-align: center
-        & > a
-          display: block
-          &.active
-            color:rgb(240,20,20)
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        &.active
+          color: rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-
+import VueResourse from 'vue-resource'
+Vue.use(VueResourse)
 import goods from 'components/goods/goods'
 import ratings from 'components/ratings/ratings'
 import seller from 'components/seller/seller'
