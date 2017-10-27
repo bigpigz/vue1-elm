b7adba398d6a7c8bbb9fdac63292d9265bd4f007
bigz <493971184@qq.com>
2017-10-27 13:51:02 +0800

    6-8 外部组件6

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,8 +17,18 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="backgound">
+      <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -30,7 +40,7 @@
       }
     },
     created(){
-        this.classMap = ['decrease','discount','special','invoice','guarantee']
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     }
   }
 </script>
@@ -39,8 +49,10 @@
   @import "../../common/stylus/mixin.styl"
   .header
     color: #fff
-    background: #7e8c8d
+    position: relative
+    background: rgba(7,17,27,0.4)
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -56,16 +68,16 @@
           .brand
             width: 30px
             height: 18px
-            vertical-align:top
+            vertical-align: top
             display: inline-block
             bg-image('brand')
-            background-size:30px 18px
-            background-repeat:no-repeat
+            background-size: 30px 18px
+            background-repeat: no-repeat
           .name
             margin-left: 6px
             font-size: 16px
             line-height: 18px
-            font-weight:bold
+            font-weight: bold
 
         .description
           margin-bottom: 10px
@@ -73,8 +85,8 @@
           font-size: 12px
         .support
           .icon
-            vertical-align:top
-            display:inline-block
+            vertical-align: top
+            display: inline-block
             width: 12px
             height: 12px
             margin-right: 4px
@@ -91,6 +103,61 @@
             &.special
               bg-image('special_1')
           .text
-            font-size: 10px
+            font-size: 12px
             line-height: 12px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background-color: rgba(0, 0, 0, 0.2);
+        text-align: center
+        .count
+          font-size: 10px
+          margin-left: 4px
+          vertical-align: top
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          font-size: 10px
+          line-height: 24px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 7px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+
+    .backgound
+      position: absolute
+      top: 0
+      left: 0
+      width 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+
 </style>
