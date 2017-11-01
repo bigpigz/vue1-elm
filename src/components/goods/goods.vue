4af05596ce27978dde6cb4a325e758f9874c8418
bigz <493971184@qq.com>
2017-11-01 09:46:43 +0800

    7-8 better-scroll运用-3

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li @click="selectMenu($index,$event)" v-for="item in goods" class="menu-item"
+            :class="{'current':currentIndex === $index}">
           </span>
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
@@ -13,7 +14,7 @@
     </div>
     <div class="foods-wrapper" v-el:food-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -28,8 +29,8 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show="food.oldPrice"
+                                                                class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -51,7 +52,9 @@
     },
     data(){
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       }
     },
     created(){
@@ -62,16 +65,54 @@
           this.goods = res.data
           this.$nextTick(() => {
             this._initScroll()
+            this._calculateHeight()
           })
 
 //          console.log(this.goods);
         }
       })
     },
+    computed: {
+      currentIndex(){
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i
+          }
+        }
+        return 0
+      }
+    },
     methods: {
       _initScroll(){
-        this.menuScroll = new BScroll(this.$els.menuWrapper, {})
-        this.foodsScroll = new BScroll(this.$els.foodWrapper, {})
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {
+          click: true
+        })
+        this.foodsScroll = new BScroll(this.$els.foodWrapper, {
+          probeType: 3
+        })
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight(){
+        let foodList = this.$els.foodWrapper.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      selectMenu(index, event){
+        if (!event._constructed) {
+          return
+        }
+        let foodList = this.$els.foodWrapper.getElementsByClassName('food-list-hook')
+        let el = foodList[index]
+        this.foodsScroll.scrollToElement(el,300)
       }
     }
 
@@ -97,6 +138,15 @@
         width: 56px
         line-height: 14px
         padding: 0 12px
+        &.current
+          position relative
+          background: #ffffff
+          font-weight: 700
+          margin-top: -1px
+          z-index: 10
+          border: 0
+          .text
+            border-none()
         .icon
           vertical-align: top
           display: inline-block
